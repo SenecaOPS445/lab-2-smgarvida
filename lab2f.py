c8354dcd81206dc6099dbfb164f41bd7ac557537
Shayna Mae Garvida <smgarvida@myseneca.ca>
2024-05-24 11:55:30 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+#Author: Shayna Mae Garvida
+#Author ID: smgarvida
+#Date Created: 2024/05/24
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
