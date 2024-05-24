b2d3c37a7d8d835f2b846b3f1b6ce0df6da145bc
Shayna Mae Garvida <smgarvida@myseneca.ca>
2024-05-24 11:40:16 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Author: Shayna Mae Garvida
+#Author ID: smgarvida
+#Date Created: 2024/05/24
+
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
